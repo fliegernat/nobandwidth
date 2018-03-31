6b9cbd3f97ede9593dab960048faa475995489ba
Nat Flieger <fliegernat@gmail.com>
2018-03-30 22:19:22 -0300

    adding config object, limiting posts on page, adding separtors

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ var app = express();
 var showdown = require('showdown');
 var converter = new showdown.Converter();
 var fs = require('fs');
+
+var config = {
+    contentDirectory: 'content',
+    maxPostsOnPage: 5
+}
+
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-    var files = fs.readdirSync('content');
+    var files = fs.readdirSync(config.contentDirectory);
     var fileInfo = files.map(function (file) {
         return {
             name: file,
-            time: fs.statSync('content/' + file).mtime.getTime()
+            time: fs.statSync(config.contentDirectory + '/' + file).mtime.getTime()
         }
     });
     var orderedFiles = fileInfo.sort(function (a, b) {
         return b.time - a.time;
     });
-    orderedFiles = orderedFiles.slice(0, 5);
+    orderedFiles = orderedFiles.slice(0, config.maxPostsOnPage);
     var renderedFiles = orderedFiles.map(function (file) {
-        var markdown = fs.readFileSync('content/' + file.name, 'utf-8');
+        var markdown = fs.readFileSync(config.contentDirectory + '/' + file.name, 'utf-8');
         return converter.makeHtml(markdown);
     });
     res.render('templates/base.pug', { 'content': renderedFiles });
